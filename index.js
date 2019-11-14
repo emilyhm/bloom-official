ac386e3bfe37df0ecf7ba814b94b740af4ed7c01
Your Nameclear <you@example.com>
2019-11-14 16:52:33 -0500

    modifying readme

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static("public"))
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("Bloom/build"));
- }
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static("Bloom/build"));
+//  }
 //  app.get("*", (req, res) =>{
 //     res.sendFile(path.join(__dirname, "./bloom/build/index.html"))
 //  })
